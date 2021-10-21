5411a78ce92c9bf369b1cea8d804c369f7200ee2
azadtamal <mrazadt@gmail.com>
2021-10-21 21:41:00 +0600

    function

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
-
-const handler = (req, res) => {
+app.get("/", (req, res) => {
   res.send("Hello World!");
-};
+});
 
-app.get("/", handler);
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
