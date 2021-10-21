c27a818f5184492f32ed796974a0f3b7f7cfa84e
azadtamal <mrazadt@gmail.com>
2021-10-21 22:37:37 +0600

    node

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World!11111111111111111");
+});
+app.get("/users", (req, res) => {
+  res.send("azad");
 });
 
 app.listen(port, () => {
