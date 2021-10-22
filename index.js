fa123637a5bb14d66369babdd2bb4a8b5b9d9a19
azadtamal <mrazadt@gmail.com>
2021-10-22 12:59:04 +0600

    function

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+var cors = require("cors");
 const app = express();
 const port = 4000;
 
+app.use(cors());
 const users = [
   { id: 1, name: "azad111111111", phone: "998877" },
   { id: 2, name: "azad222222222", phone: "998877" },
@@ -13,22 +15,26 @@ app.get("/", (req, res) => {
   res.send("Hello World!11111111111111111");
 });
 
-app.get("/users", (req, res) => {
-  const search = req.query.search;
-  if (search) {
-    const searchResult = users.filter((user) =>
-      user.name.toLocaleLowerCase().includes(search)
-    );
-    res.send(searchResult);
-  }
-});
-
-app.get("/users/:id", (req, res) => {
-  const id = req.params.id;
-  const user = users[id];
-  res.send(user);
-});
-
-// app.listen(port, () => {
-//   console.log(`Example app listening at http://localhost:${port}`);
+// app.get("/users", (req, res) => {
+//   const search = req.query.search;
+//   if (search) {
+//     const searchResult = users.filter((user) =>
+//       user.name.toLocaleLowerCase().includes(search)
+//     );
+//     res.send(searchResult);
+//   }
 // });
+
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
+// app.get("/users/:id", (req, res) => {
+//   const id = req.params.id;
+//   const user = users[id];
+//   res.send(user);
+// });
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
